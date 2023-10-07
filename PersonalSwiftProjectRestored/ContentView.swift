b31b9f720a0929417284a3e915c5e20caa7ad00d
Andrey-Boyarov <a.boyarov@icloud.com>
2023-10-07 14:02:21 +0400

    Initial Commit

---
+++ PersonalSwiftProjectRestored/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PersonalSwiftProjectRestored
+//
+//  Created by Andrey Boyarov on 07.10.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PersonalSwiftProjectRestored/PersonalSwiftProjectRestoredApp.swift
@@ -0,0 +1,17 @@
+//
+//  PersonalSwiftProjectRestoredApp.swift
+//  PersonalSwiftProjectRestored
+//
+//  Created by Andrey Boyarov on 07.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct PersonalSwiftProjectRestoredApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
